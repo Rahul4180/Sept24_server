514148f0f6692d86627e6e6b912d616dedc2094d
Rahul4180 <rahul.malik0890687@gmail.com>
2024-09-17 16:24:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const cron = require('node-cron');
 const axios = require('axios');
 
 // Your server URL
-const serverUrl = 'https://july-22-server.onrender.com/';
+const serverUrl = 'https://sept24-server.onrender.com';
 
 // Define the cron job
 cron.schedule('*/5 * * * *', async () => {
